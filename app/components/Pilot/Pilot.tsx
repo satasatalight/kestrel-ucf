975c9469e256fc534af85b578fe064aff27c410a
kamillamamatova <kamillamamatova.km@gmail.com>
2025-04-10 19:54:58 -0400

    Testing

--- app/components/Pilot/Pilot.tsx
+++ app/components/Pilot/Pilot.tsx
@@ -3,9 +3,9 @@ import "./Pilot.css";
 export default function Pilot() {
   return (
     <div className="flex flex-col gap-2 items-center justify-center">
-      <h1 className="text-center text-white text-3xl">Welcome to</h1>
-      <h1 className="proj-name text-[5rem] text-center font-sans text-shadow-[2px_1px_rgb(0,0,0)]">
-        Kestrel UCF
+      <h1 className="text-center text-white text-4xl">Welcome to</h1>
+      <h1 className="proj-name text-[5rem] text-center font-sans text-shadow-[3px_2px_rgb(0,0,0)]">
+        Kestrel UCFF
       </h1>
     </div>
   );
