8a8031dc885d86cb68af025745c4441c9e93fe78
satasatalight <satalight.x@gmail.com>
2025-06-03 18:34:38 -0400

    build member card component

---
+++ app/teams/_components/MemberCard.tsx
@@ -0,0 +1,54 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link"
+import{
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import { link } from "fs";
+
+export interface MemberCardProps{
+    name: string;
+    role: string;
+    description: string;
+    image: StaticImageData;
+    links: Array<LinkProps>;
+}
+
+interface LinkProps{
+    type: string;
+    href: string;
+}
+
+const MemberCard = ({name, description, role, image, links} : MemberCardProps) => {
+    const LinksElement = links.map((link) => {
+        return <a className="text-blue-500 decoration-blue-500 underline pe-2" href={link.href}>{link.type}</a>
+    })
+
+    return <Card className="flex justify-between text-left overflow-hidden">
+        <CardContent className="pb-0 flex flex-col justify-between">
+            <CardHeader className="pb-3 pt-4">
+                <CardTitle className="text-3xl">
+                    {name}
+                </CardTitle>
+                <CardDescription className="text-1xl pb-2">
+                    {role}
+                </CardDescription>
+                <p className="mb-2">
+                    {description}
+                </p>
+            </CardHeader>
+            
+            <CardFooter className="border-gray-500 border-t w-100 mx-6 px-0 pt-1">
+                {LinksElement}
+            </CardFooter>
+        </CardContent>
+
+        <Image className="max-w-4/10 rounded-l-4xl" src={image} alt={name}/>
+    </Card>
+}
+
+export default MemberCard;
--- app/teams/sensors/page.tsx
+++ app/teams/sensors/page.tsx
@@ -1,7 +1,71 @@
 import React from "react";
+import placeholder from "@/public/ArduinoBoard.png";
+import MemberCard, { MemberCardProps } from "../_components/MemberCard";
 
 const SensorsTeamPage = () => {
-  return <div>Sensors TeamsPage</div>;
+  const members: MemberCardProps[] = [
+    {
+      name: "name1",
+      role: "role",
+      description: "Lorem ",
+      image: placeholder,
+      links: [
+        {
+          type: "cover_letter",
+          href: "./"
+        },
+        {
+          type: "github",
+          href: "./"
+        }
+      ]
+    },
+    {
+      name: "name2",
+      role: "role",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      image: placeholder,
+      links: [
+        {
+          type: "linkedin",
+          href: "./"
+        },
+        {
+          type: "resume",
+          href: "./"
+        }
+      ]
+    },
+  ];
+
+  return <div className="bg-linear-to-t from-purple-900 to-fuchsia-950 text-neutral-200">
+    <h1 className="pt-20 text-5xl text-center font-bold font-mono">
+      Kestrel's Sensors Team
+    </h1>
+
+    <div className="container mx-auto max-w-5xl">
+
+      {/* Rundown */}
+      <p className="pt-10 text-base text-center">
+        placeholder team rundown
+      </p>
+
+      {/* Highlighted Devlogs */}
+      <h2 className="pt-10 text-3xl font-light font-mono">Highlighted Devlogs:</h2>
+      
+      {/* Team Member Overviews */}
+      <h2 className="pt-10 text-3xl font-light font-mono">Team Members:</h2>
+      <div className="flex justify-center items-center mx-3">
+        <div className="flex flex-wrap w-full pt-6 pb-15 gap-6">
+          {members.map((member) => {
+
+            return <MemberCard key={member.name} {...member}/>
+          })}
+        </div>
+      </div>
+
+    </div>
+  </div>;
 };
 
 export default SensorsTeamPage;
