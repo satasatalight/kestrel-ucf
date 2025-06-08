ca2fa59addec27af87775951d9678abff50b9ee6
satalight <114524244+satasatalight@users.noreply.github.com>
2025-06-08 16:40:26 -0400

    move member data to external file

--- app/teams/embedded/page.tsx
+++ app/teams/embedded/page.tsx
@@ -1,48 +1,8 @@
 import React from "react";
-import placeholder from "@/public/ArduinoBoard.png";
-import MemberCard, { MemberCardProps } from "../_components/MemberCard";
+import MemberCard from "../_components/MemberCard";
+import members from "./members"
 
 const SensorsTeamPage = () => {
-  // placeholder data
-  const members: MemberCardProps[] = [
-    {
-      name: "name1",
-      role: "role",
-      description: "Lorem ",
-      image: placeholder,
-      links: [
-        {
-          type: "cover_letter",
-          href: "iorhgirhgi"
-        },
-        {
-          type: "github",
-          href: "https://github.com"
-        }
-      ]
-    },
-    {
-      name: "name2",
-      role: "role",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      image: placeholder,
-      links: [
-        {
-          type: "linkedin",
-          href: "https://linkedin.com"
-        },
-        {
-          type: "resume",
-          href: "resumeresume"
-        },
-        {
-          type: "misc link",
-          href: "uihiuhuihoihoihih"
-        }
-      ]
-    },
-  ];
-
   return <div className="bg-gradient-to-t from-slate-700 to-slate-900 text-neutral-200 h-[100vh]">
     <h1 className="pt-20 text-5xl text-center font-bold font-mono">
       Kestrel's Embedded Software Team
